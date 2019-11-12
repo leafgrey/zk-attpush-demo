2b01d74c44715123cbd5414f8e93c3ef2de22694
zchcpy <zghnzch@163.com>
2019-11-12 14:04:12 +0800

    整理格式

--- src/main/java/com/zkteco/attpush/att/AttPushProcessor.java
+++ src/main/java/com/zkteco/attpush/att/AttPushProcessor.java
@@ -1,7 +1,10 @@
 package com.zkteco.attpush.att;
+import com.google.common.collect.Maps;
+import com.zkteco.constant.ConstantStr;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.springframework.stereotype.Controller;
@@ -17,13 +20,15 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 /**
  * 所有的设备请求都会在url参数里携带SN，这是设备序列号(serial number的缩写)，每个设备唯一标识
+ * @author zch
  */
 @Controller
 @RequestMapping("/iclock")
-public class AttPushProccesor {
+public class AttPushProcessor {
+	private final static Logger myLog = Logger.getRootLogger();
 	private static Map<String, List<String>> cmdMap = new HashMap<>();
-	public static Map<String, String> formateStrToMap(String data, String split) {
-		Map<String, String> dataMap = new HashMap<String, String>();
+	private static Map<String, String> formateStrToMap(String data, String split) {
+		Map<String, String> dataMap = Maps.newHashMapWithExpectedSize(3);
 		String[] dataArray = data.split(split);
 		for (String d : dataArray) {
 			String[] oneData = d.split("=", 2);
@@ -46,8 +51,8 @@ public class AttPushProccesor {
 	}
 	/**
 	 * 保存base64图片
-	 * @param fileName
-	 * @param base64ImgStr
+	 * @param fileName String
+	 * @param base64ImgStr String
 	 */
 	private static void saveFile(String fileName, String base64ImgStr) {
 		try {
@@ -55,10 +60,12 @@ public class AttPushProccesor {
 			String fullPath = "D://photo";
 			File file = new File(new File(fullPath), fileName);
 			if(!file.getParentFile().exists()) {
-				file.getParentFile().mkdirs();
+				boolean idMkdir = file.getParentFile().mkdirs();
+				myLog.debug(idMkdir);
 			}
 			if(file.exists()) {
-				file.delete();
+				boolean isDelete = file.delete();
+				myLog.debug(isDelete);
 			}
 			OutputStream out = new FileOutputStream(file);
 			out.write(java.util.Base64.getDecoder().decode(base64ImgStr));
@@ -74,43 +81,53 @@ public class AttPushProccesor {
 	 * 格式为 /iclock/cdata?options=all&language=xxxx&pushver=xxxx
 	 */
 	@RequestMapping(value = "/cdata", params = {"options", "language", "pushver"})
-	public void init(String SN, String options, String language, String pushver, @RequestParam(required = false) String PushOptionsFlag, HttpServletResponse response) {
+	public void init(String sn, String options, String language, String pushver,
+	                 @RequestParam(required = false) String pushOptionsFlag, HttpServletResponse response) {
 		System.out.println("options=" + options + "....language=" + language + "....pushver=" + pushver);
 		try {
 			System.out.println("考勤机初始化请求进来了......");
-			loadCmd(SN);//加载命令
-			String initOptions = initOptions(SN, PushOptionsFlag);
+			//加载命令
+			loadCmd(sn);
+			String initOptions = initOptions(sn, pushOptionsFlag);
 			System.out.println("返回给考勤机的初始化参数为...." + initOptions);
-			response.getWriter().write(initOptions);//返回成功以后设备会发送心跳请求
-			//response.getWriter().write("UNKNOWN DEVICE");当返回这个的时候，设备会每2秒重新发本请求，直到返回OK为止
+			//返回成功以后设备会发送心跳请求
+			response.getWriter().write(initOptions);
+			// 当返回这个的时候，设备会每2秒重新发本请求，直到返回OK为止
+			// response.getWriter().write("UNKNOWN DEVICE")
 		}
 		catch (IOException e) {
-			e.printStackTrace();
+			myLog.error(e.getMessage(),e);
 		}
 	}
 	/**
 	 * 2，心跳请求，会从服务器拿到命令返回给设备
 	 */
 	@RequestMapping("/getrequest")
-	public void heartBeat(String SN, HttpServletResponse response, HttpServletRequest request) {
+	public void heartBeat(String sn, HttpServletResponse response, HttpServletRequest request) {
 		System.out.println(request.getParameter("SN"));
 		StringBuffer sb = new StringBuffer("OK");
-		List<String> cmds = cmdMap.get(SN);
-		if(cmds == null) {//等于空说明从来没加载过，如果初始化加载过了，此时应该不为Null 只是size为0
-			loadCmd(SN);
-			cmds = cmdMap.get(SN);
+		List<String> cmds = cmdMap.get(sn);
+		//等于空说明从来没加载过，如果初始化加载过了，此时应该不为Null 只是size为0
+		if(cmds == null) {
+			loadCmd(sn);
+			cmds = cmdMap.get(sn);
 		}
 		if(cmds != null && cmds.size() > 0) {
-			sb.setLength(0);//如果有命令就不返回OK了
-			cmds.stream().forEach(cmd -> sb.append(cmd).append("\r\n\r\n"));
+			//如果有命令就不返回OK了
+			sb.setLength(0);
+			// 原代码
+			// cmds.stream().forEach(cmd -> sb.append(cmd).append("\r\n\r\n"))
+			// 20191112 zch 修改
+			cmds.forEach(cmd -> sb.append(cmd).append("\r\n\r\n"));
 		}
 		System.out.println("心跳的返回结果为...." + sb);
 		try {
-			cmdMap.get(SN).clear();//处理完以后立刻将集合清空，实际开发中应该是在/devicecmd这个请求里完成
+			//处理完以后立刻将集合清空，实际开发中应该是在/devicecmd这个请求里完成
+			cmdMap.get(sn).clear();
 			response.getWriter().write(sb.toString());
 		}
 		catch (IOException e) {
-			e.printStackTrace();
+			myLog.error(e.getMessage(),e);
 		}
 	}
 	/**
@@ -132,7 +149,7 @@ public class AttPushProccesor {
 	 */
 	@RequestMapping("/devicecmd")
 	public void handleCmd(String sn, @RequestBody String data, HttpServletResponse response) {
-		System.out.println("设备处理完命令以后的返回结果..." + data);
+		System.out.println(sn+"设备处理完命令以后的返回结果..." + data);
 		try {
 			response.getWriter().write("OK");
 		}
@@ -141,15 +158,16 @@ public class AttPushProccesor {
 		}
 	}
 	@RequestMapping(value = "/cdata", params = "table")
-	public void handleRtData(HttpServletRequest request, HttpServletResponse response, String SN, String table) {
-		//System.out.println("设备上传上来的数据...."+data+"....table..."+table);
+	public void handleRtData(HttpServletRequest request, HttpServletResponse response, String sn, String table) {
+		//System.out.println("设备上传上来的数据...."+data+"....table..."+table)
+		System.out.println(sn+"设备上传上来的数据...table..."+table);
 		String data = "";
 		ByteArrayOutputStream bos = null;
 		byte[] b = new byte[1024];
 		try {
 			InputStream is = request.getInputStream();
 			bos = new ByteArrayOutputStream();
-			int len = 0;
+			int len;
 			while ((len = is.read(b)) != -1) {
 				bos.write(b, 0, len);
 			}
@@ -158,18 +176,23 @@ public class AttPushProccesor {
 		catch (IOException e) {
 			e.printStackTrace();
 		}
-		if("ATTPHOTO".equals(table)) {
+		if(ConstantStr.ATT_PHOTO.equals(table)) {
 			//如果是考勤图片上传,此处重点演示如何处理上传到软件里的照片
 			try {
-				String photo = getAttPhotoDataInfo(data, bos.toByteArray()).getString("static/photo");
+				// String photo = getAttPhotoDataInfo(data, bos.toByteArray()).getString("static/photo")
+				String photo = null;
+				if(bos != null) {
+					photo = Objects.requireNonNull(getAttPhotoDataInfo(data, bos.toByteArray())).getString("static/photo");
+				}
 				saveFile(System.currentTimeMillis() + ".jpg", photo);
 			}
 			catch (JSONException e) {
-				e.printStackTrace();
+				myLog.error(e.getMessage(),e);
 			}
 		}
-		if("OPERLOG".equals(table)) {
-			if(data.startsWith("BIOPHOTO")) {//处理人员上传上拉的比对照片模板
+		if(ConstantStr.OPER_LOG.equals(table)) {
+			//处理人员上传上拉的比对照片模板
+			if(data.startsWith(ConstantStr.BIO_PHOTO)) {
 				Map<String, String> preInfoMap = formateStrToMap(data, "\t");
 				System.out.println("preInfoMap..." + preInfoMap);
 				//处理上传上来的比对照片
@@ -188,7 +211,7 @@ public class AttPushProccesor {
 			List<String> cmdList = new ArrayList<>();
 			File file = ResourceUtils.getFile("classpath:cmd.txt");
 			BufferedReader br = new BufferedReader(new FileReader(file));
-			String cmd = "";
+			String cmd;
 			while ((cmd = br.readLine()) != null) {
 				if(!cmd.startsWith("#")) {
 					System.out.println("加载进内存的命令是...." + cmd);
@@ -198,28 +221,32 @@ public class AttPushProccesor {
 			cmdMap.put(sn, cmdList);
 		}
 		catch (Exception e) {
-			e.printStackTrace();
+			myLog.error(e.getMessage(),e);
 		}
 	}
 	/**
 	 * 设备通电以后连接到服务器，需要返回的初始化参数
-	 * @param sn
-	 * @param PushOptionsFlag
-	 * @return
+	 * @param sn String
+	 * @param pushOptionsFlag String
+	 * @return String
 	 */
-	private String initOptions(String sn, String PushOptionsFlag) {
-		StringBuffer devOptions = new StringBuffer();
-		devOptions.append("GET OPTION FROM: " + sn);
+	private String initOptions(String sn, String pushOptionsFlag) {
+		StringBuilder devOptions = new StringBuilder();
+		devOptions.append("GET OPTION FROM: ").append(sn);
 		// + "\nStamp=" + devInfo.devInfoJson.getString("Stamp")
 		devOptions.append("\nATTLOGStamp=0");
 		devOptions.append("\nOPERLOGStamp=0");
 		devOptions.append("\nBIODATAStamp=0");
 		devOptions.append("\nATTPHOTOStamp=0");
-		devOptions.append("\nErrorDelay=10");//断网重连
-		devOptions.append("\nDelay=5");//心跳间隔
-		devOptions.append("\nTimeZone=8");//时区
-		devOptions.append("\nRealtime=1");//实时上传
-		if(PushOptionsFlag != null && PushOptionsFlag.equals("1")) {
+		//断网重连
+		devOptions.append("\nErrorDelay=10");
+		//心跳间隔
+		devOptions.append("\nDelay=5");
+		//时区
+		devOptions.append("\nTimeZone=8");
+		//实时上传
+		devOptions.append("\nRealtime=1");
+		if(ConstantStr.NUM_STR1.equals(pushOptionsFlag)) {
 			// 支持参数单独获取的才要把需要获取的参数回传给设备 modifeid by max 20170926
 			devOptions.append("\nPushOptions=RegDeviceType,FingerFunOn,FaceFunOn,FPVersion,FaceVersion,NetworkType,HardwareId3,HardwareId5,HardwareId56,LicenseStatus3,LicenseStatus5,LicenseStatus56");
 		}
---
+++ src/main/java/com/zkteco/constant/ConstantStr.java
@@ -0,0 +1,17 @@
+package com.zkteco.constant;
+/***
+ * @class ConstantStr
+ * @description String 类型常量
+ * @author zch
+ * @date 2019/11/12
+ * @version V0.0.1.201911121402.01
+ * @modfiyDate 201911121402
+ * @createDate 201911121402
+ * @package com.zkteco.constant
+ */
+public class ConstantStr {
+	public final static String ATT_PHOTO = "ATTPHOTO";
+	public final static String NUM_STR1 = "1";
+	public final static String OPER_LOG = "OPERLOG";
+	public final static String BIO_PHOTO = "BIOPHOTO";
+}
